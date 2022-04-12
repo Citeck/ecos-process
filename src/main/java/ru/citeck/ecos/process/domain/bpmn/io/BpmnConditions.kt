5ddbf35631f2cb87d72b316e059a8acc592f55be
roman.makarskiy <roman.makarskiy@citeck.ru>
2022-04-12 11:54:41 +0300

    ECOSENT-2222 - ECOS bpmn camunda

--- src/main/java/ru/citeck/ecos/process/domain/bpmn/io/BpmnConditions.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/io/BpmnConditions.kt
@@ -1,5 +1,5 @@
 package ru.citeck.ecos.process.domain.bpmn.io
 
-fun propMandatoryError(attr: String, dto: Any): Nothing = throw IllegalStateException(
+fun propMandatoryError(attr: Any, dto: Any): Nothing = throw IllegalStateException(
     "Attribute $attr is a mandatory for $dto"
 )
