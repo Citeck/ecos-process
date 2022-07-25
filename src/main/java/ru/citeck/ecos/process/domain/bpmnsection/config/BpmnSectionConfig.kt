51c19a846af7f776e7b1cfe26b80e5ff670b5747
Pavel Simonov <pavel.simonov@citeck.ru>
2022-07-25 13:29:41 +0700

    ECOSCOM-4639 - ecos-data enhancement

--- src/main/java/ru/citeck/ecos/process/domain/bpmnsection/config/BpmnSectionConfig.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmnsection/config/BpmnSectionConfig.kt
@@ -7,18 +7,15 @@ import ru.citeck.ecos.data.sql.domain.DbDomainConfig
 import ru.citeck.ecos.data.sql.domain.DbDomainFactory
 import ru.citeck.ecos.data.sql.dto.DbTableRef
 import ru.citeck.ecos.data.sql.records.DbRecordsDaoConfig
-import ru.citeck.ecos.data.sql.records.perms.DefaultDbPermsComponent
 import ru.citeck.ecos.data.sql.service.DbDataServiceConfig
 import ru.citeck.ecos.model.lib.type.service.utils.TypeUtils
-import ru.citeck.ecos.records3.RecordsService
 import ru.citeck.ecos.records3.record.dao.RecordsDao
 import ru.citeck.ecos.webapp.api.datasource.JdbcDataSource
 import ru.citeck.ecos.webapp.lib.spring.context.datasource.EcosDataSourceManager
 
 @Configuration
 class BpmnSectionConfig(
-    private val dbDomainFactory: DbDomainFactory,
-    private val recordsService: RecordsService
+    private val dbDomainFactory: DbDomainFactory
 ) {
 
     companion object {
@@ -28,7 +25,6 @@ class BpmnSectionConfig(
     @Bean
     fun bpmnSectionRepoDao(dataSourceManager: EcosDataSourceManager): RecordsDao {
         val dataSource = dataSourceManager.getDataSource("eproc", JdbcDataSource::class.java).getJavaDataSource()
-        val permsComponent = DefaultDbPermsComponent(recordsService)
 
         val typeRef = TypeUtils.getTypeRef("bpmn-section")
         val recordsDao = dbDomainFactory.create(
@@ -48,8 +44,7 @@ class BpmnSectionConfig(
                     }
                 )
                 .build()
-        ).withPermsComponent(permsComponent)
-            .withDataSource(DbDataSourceImpl(dataSource))
+        ).withDataSource(DbDataSourceImpl(dataSource))
             .build()
         return recordsDao
     }
