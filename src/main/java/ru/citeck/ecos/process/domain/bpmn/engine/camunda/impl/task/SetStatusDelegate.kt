9d97ac0f39a0960e5f913eb2fa511fe4045d072b
Roman Makarskiy <roman.makarskiy@citeck.ru>
2022-09-12 17:50:14 +0300

    ECOSENT-2302 - Camunda services unit test
    - set status bpmn test

--- src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/impl/task/SetStatusDelegate.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/impl/task/SetStatusDelegate.kt
@@ -4,22 +4,20 @@ import org.camunda.bpm.engine.delegate.DelegateExecution
 import org.camunda.bpm.engine.delegate.Expression
 import org.camunda.bpm.engine.delegate.JavaDelegate
 import ru.citeck.ecos.context.lib.auth.AuthContext
-import ru.citeck.ecos.model.lib.status.constants.StatusConstants
 import ru.citeck.ecos.process.app.AppContext
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.getNotBlankDocumentRef
+import ru.citeck.ecos.process.domain.bpmn.engine.camunda.services.CamundaStatusSetter
 import ru.citeck.ecos.records2.RecordRef
-import ru.citeck.ecos.records3.RecordsService
-import ru.citeck.ecos.records3.record.atts.dto.RecordAtts
 
 class SetStatusDelegate : JavaDelegate {
 
     var status: Expression? = null
 
-    private lateinit var recordsService: RecordsService
+    private lateinit var statusSetter: CamundaStatusSetter
     private lateinit var document: RecordRef
 
     private fun init(execution: DelegateExecution) {
-        recordsService = AppContext.getBean(RecordsService::class.java)
+        statusSetter = AppContext.getBean(CamundaStatusSetter::class.java)
         document = execution.getNotBlankDocumentRef()
     }
 
@@ -32,10 +30,7 @@ class SetStatusDelegate : JavaDelegate {
         }
 
         AuthContext.runAsSystem {
-            val recordAtts = RecordAtts(document)
-            recordAtts.setAtt(StatusConstants.ATT_STATUS, statusValue)
-
-            recordsService.mutate(recordAtts)
+            statusSetter.setStatus(document, statusValue)
         }
     }
 }
---
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/services/CamundaStatusSetter.kt
@@ -0,0 +1,21 @@
+package ru.citeck.ecos.process.domain.bpmn.engine.camunda.services
+
+import org.springframework.stereotype.Component
+import ru.citeck.ecos.model.lib.status.constants.StatusConstants
+import ru.citeck.ecos.records2.RecordRef
+import ru.citeck.ecos.records3.RecordsService
+import ru.citeck.ecos.records3.record.atts.dto.RecordAtts
+
+@Component
+class CamundaStatusSetter(
+    private val recordsService: RecordsService
+) {
+
+    fun setStatus(documentRef: RecordRef, status: String) {
+        val recordAtts = RecordAtts(documentRef)
+        recordAtts.setAtt(StatusConstants.ATT_STATUS, status)
+
+        recordsService.mutate(recordAtts)
+    }
+
+}
