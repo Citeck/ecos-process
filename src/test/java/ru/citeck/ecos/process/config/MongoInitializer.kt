c13bab7a795c48ff32ff5cf2d79f6939fca9cd0d
Roman Makarskiy <romanchabest55@gmail.com>
2023-02-09 11:53:55 +0300

    Replace in mem mongo to test containers

---
+++ src/test/java/ru/citeck/ecos/process/config/MongoInitializer.kt
@@ -0,0 +1,27 @@
+package ru.citeck.ecos.process.config
+
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.test.context.support.TestPropertySourceUtils
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+
+@Testcontainers
+class MongoInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+    @Container
+    val container = MongoDBContainer(DockerImageName.parse("mongo:4.2"))
+
+    init {
+        container.start()
+    }
+
+    override fun initialize(context: ConfigurableApplicationContext) {
+        TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+            context,
+            "spring.data.mongodb.uri=" + container.replicaSetUrl
+        )
+    }
+}
