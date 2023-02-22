f03e469138708f587de17ef55ee828853455f6a1
Roman Makarskiy <romanchabest55@gmail.com>
2023-02-22 15:35:02 +0300

    Add ability to transform data to DataValue

---
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/services/beans/CamundaProcessBpmnDataValueScriptComponent.kt
@@ -0,0 +1,27 @@
+package ru.citeck.ecos.process.domain.bpmn.engine.camunda.services.beans
+
+import org.springframework.stereotype.Component
+import ru.citeck.ecos.process.domain.bpmn.engine.camunda.impl.variables.convert.BpmnDataValue
+
+@Component("DataValue")
+class CamundaProcessBpmnDataValueScriptComponent : CamundaProcessEngineService {
+    override fun getKey(): String {
+        return "DataValue"
+    }
+
+    fun of(content: Any?): BpmnDataValue {
+        return BpmnDataValue.create(content)
+    }
+
+    fun createObj(): BpmnDataValue {
+        return BpmnDataValue.createObj()
+    }
+
+    fun createArr(): BpmnDataValue {
+        return BpmnDataValue.createArr()
+    }
+
+    fun createStr(value: Any?): BpmnDataValue {
+        return BpmnDataValue.createStr(value)
+    }
+}
