8bdbadc791c3e789ba9e2746d3fbb305602224f8
Roman Makarskiy <romanchabest55@gmail.com>
2023-05-30 12:34:46 +0300

    ECOSCOM-5051 - BPMN - Add the ability to change the performer
    - fix patch to use list pages

--- src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/patch/BpmnTaskSingleCandidateUserToAssigneePatch.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/patch/BpmnTaskSingleCandidateUserToAssigneePatch.kt
@@ -2,13 +2,16 @@ package ru.citeck.ecos.process.domain.bpmn.engine.camunda.patch
 
 import mu.KotlinLogging
 import org.camunda.bpm.engine.TaskService
+import org.camunda.bpm.engine.task.Task
 import org.springframework.stereotype.Component
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.splitToUserGroupCandidates
 import ru.citeck.ecos.webapp.lib.patch.annotaion.EcosPatch
 import java.util.concurrent.Callable
 
+private const val BATCH_SIZE = 100
+
 @Component
-@EcosPatch("bpmn-task-single-candidate-user-to-assignee", "2023-05-02T00:00:00Z")
+@EcosPatch("bpmn-task-single-candidate-user-to-assignee_5", "2023-05-02T00:00:00Z")
 class BpmnTaskSingleCandidateUserToAssigneePatch(
     private val camundaTaskService: TaskService
 ) : Callable<Any> {
@@ -19,32 +22,53 @@ class BpmnTaskSingleCandidateUserToAssigneePatch(
 
     override fun call(): Any {
 
-        val tasksWithCandidateUsers = camundaTaskService.createTaskQuery()
+        var firstResult = 0
+        var totalFoundFilteredTasks = 0
+        var tasksWithCandidateUsers = findUnassignedTasksWithCandidateUsers(firstResult)
+
+        while (tasksWithCandidateUsers.isNotEmpty()) {
+
+            log.info {
+                "Found ${tasksWithCandidateUsers.size} for BpmnTaskSingleCandidateUserToAssigneePatch patch. " +
+                    "FirstResult size: $firstResult "
+            }
+
+            val filtered = tasksWithCandidateUsers
+                .filter {
+                    val links = camundaTaskService.getIdentityLinksForTask(it.id)
+                    val (candidateUsers, candidateGroups) = links.splitToUserGroupCandidates()
+                    it.assignee == null && candidateUsers.size == 1 && candidateGroups.isEmpty()
+                }
+
+            log.info { "Filtered ${filtered.size} for BpmnTaskSingleCandidateUserToAssigneePatch patch" }
+
+            filtered.forEach {
+                log.info { "Patch SingleCandidateUser task ${it.id}" }
+
+                val assigneeUser = camundaTaskService.getIdentityLinksForTask(it.id).first().userId
+                it.assignee = assigneeUser
+                camundaTaskService.deleteCandidateUser(it.id, assigneeUser)
+            }
+
+            totalFoundFilteredTasks += filtered.size
+            firstResult += tasksWithCandidateUsers.size
+
+            tasksWithCandidateUsers = findUnassignedTasksWithCandidateUsers(firstResult)
+        }
+
+        val msg = "Patched $totalFoundFilteredTasks tasks of found $firstResult tasks"
+
+        log.info { msg }
+
+        return msg
+    }
+
+    private fun findUnassignedTasksWithCandidateUsers(skip: Int): List<Task> {
+        return camundaTaskService.createTaskQuery()
             .withCandidateUsers()
             .withoutCandidateGroups()
             .taskUnassigned()
             .initializeFormKeys()
-            .list()
-
-        log.info { "Found ${tasksWithCandidateUsers.size} for BpmnTaskSingleCandidateUserToAssigneePatch patch" }
-
-        val filtered = tasksWithCandidateUsers
-            .filter {
-                val links = camundaTaskService.getIdentityLinksForTask(it.id)
-                val (candidateUsers, candidateGroups) = links.splitToUserGroupCandidates()
-                it.assignee == null && candidateUsers.size == 1 && candidateGroups.isEmpty()
-            }
-
-        log.info { "Filtered ${filtered.size} for BpmnTaskSingleCandidateUserToAssigneePatch patch" }
-
-        filtered.forEach {
-            log.info { "Patch SingleCandidateUser task ${it.id}" }
-
-            val assigneeUser = camundaTaskService.getIdentityLinksForTask(it.id).first().userId
-            it.assignee = assigneeUser
-            camundaTaskService.deleteCandidateUser(it.id, assigneeUser)
-        }
-
-        return "Patched ${filtered.size} tasks"
+            .listPage(skip, BATCH_SIZE)
     }
 }
