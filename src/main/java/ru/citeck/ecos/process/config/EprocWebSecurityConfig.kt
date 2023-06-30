07fdffd19a9163007b72e1f67bb44478b58a15fe
Roman Makarskiy <romanchabest55@gmail.com>
2023-06-30 17:17:49 +0300

    ECOSCOM-5080 - BPMN external tasks
    - secure engine-rest endpoint

---
+++ src/main/java/ru/citeck/ecos/process/config/EprocWebSecurityConfig.kt
@@ -0,0 +1,24 @@
+package ru.citeck.ecos.process.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import ru.citeck.ecos.context.lib.auth.AuthRole
+import ru.citeck.ecos.webapp.lib.spring.context.security.SecurityConfiguration
+
+@Order(90)
+@Configuration
+class EprocWebSecurityConfig(
+    val existingConfig: SecurityConfiguration
+) : WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        existingConfig.configure(http)
+
+        http
+            .authorizeRequests()
+            .antMatchers("/engine-rest/**").hasAnyAuthority(AuthRole.ADMIN, AuthRole.SYSTEM)
+    }
+}
