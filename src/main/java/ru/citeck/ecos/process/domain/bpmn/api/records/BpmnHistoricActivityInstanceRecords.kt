0f08826a6c7075ec9bb86344c3bde788dc785fbc
Roman Makarskiy <romanchabest55@gmail.com>
2023-10-16 21:51:55 +0300

    ECOSENT-2735 - Records API for Bpmn Admin

---
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/api/records/BpmnHistoricActivityInstanceRecords.kt
@@ -0,0 +1,47 @@
+package ru.citeck.ecos.process.domain.bpmn.api.records
+
+import org.camunda.bpm.engine.HistoryService
+import org.camunda.bpm.engine.history.HistoricActivityInstance
+import org.springframework.stereotype.Component
+import ru.citeck.ecos.commons.data.MLText
+import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
+import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
+import ru.citeck.ecos.records3.record.dao.atts.RecordAttsDao
+
+@Component
+class BpmnHistoricActivityInstanceRecords(
+    private val camundaHistoryService: HistoryService
+) : AbstractRecordsDao(), RecordAttsDao {
+
+    companion object {
+        const val ID = "bpmn-historic-activity-instance"
+    }
+
+    override fun getId() = ID
+
+    override fun getRecordAtts(recordId: String): Any? {
+        val activityInstance: HistoricActivityInstance = camundaHistoryService.createHistoricActivityInstanceQuery()
+            .activityInstanceId(recordId)
+            .singleResult() ?: error("Activity instance with id $recordId not found")
+
+        return ActivityInstanceRecord(activityInstance)
+    }
+
+    private inner class ActivityInstanceRecord(
+        private val historicActivityInstance: HistoricActivityInstance,
+
+        var id: String = historicActivityInstance.id
+    ) {
+
+        @AttName(".disp")
+        fun getDisp(): MLText {
+            return MLText(historicActivityInstance.activityName ?: "")
+        }
+
+        @AttName("activityId")
+        fun getActivityId(): String {
+            return historicActivityInstance.activityId
+        }
+
+    }
+}
--- src/main/java/ru/citeck/ecos/process/domain/bpmn/api/records/BpmnVariableInstanceRecords.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/api/records/BpmnVariableInstanceRecords.kt
@@ -1,27 +1,34 @@
 package ru.citeck.ecos.process.domain.bpmn.api.records
 
+import ecos.com.fasterxml.jackson210.databind.JavaType
 import mu.KotlinLogging
 import org.camunda.bpm.engine.RuntimeService
 import org.camunda.bpm.engine.impl.persistence.entity.VariableInstanceEntity
 import org.camunda.bpm.engine.runtime.VariableInstanceQuery
 import org.camunda.bpm.engine.variable.value.SerializableValue
 import org.springframework.stereotype.Component
+import ru.citeck.ecos.commons.json.Json
 import ru.citeck.ecos.records2.predicate.PredicateUtils
 import ru.citeck.ecos.records2.predicate.model.Predicate
+import ru.citeck.ecos.records3.record.atts.dto.LocalRecordAtts
 import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
 import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
 import ru.citeck.ecos.records3.record.dao.atts.RecordAttsDao
+import ru.citeck.ecos.records3.record.dao.delete.DelStatus
+import ru.citeck.ecos.records3.record.dao.delete.RecordDeleteDao
+import ru.citeck.ecos.records3.record.dao.mutate.RecordMutateDao
 import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
 import ru.citeck.ecos.records3.record.dao.query.dto.res.RecsQueryRes
 import ru.citeck.ecos.webapp.api.constants.AppName
 import ru.citeck.ecos.webapp.api.entity.EntityRef
 import java.nio.charset.StandardCharsets
+import java.util.*
 
 @Component
 class BpmnVariableInstanceRecords(
     private val camundaRuntimeService: RuntimeService
-) : AbstractRecordsDao(), RecordsQueryDao, RecordAttsDao {
+) : AbstractRecordsDao(), RecordsQueryDao, RecordAttsDao, RecordMutateDao, RecordDeleteDao {
 
     companion object {
         private val log = KotlinLogging.logger {}
@@ -30,9 +37,108 @@ class BpmnVariableInstanceRecords(
 
         private const val ATT_NAME = "name"
         private const val ATT_TYPE = "type"
+        private const val ATT_VALUE = "value"
+        private const val ATT_OBJECT_TYPE_NAME = "objectTypeName"
+        private const val ATT_EXECUTION_ID = "executionId"
+
+        private const val TYPE_STRING = "string"
+        private const val TYPE_DATE = "date"
+        private const val TYPE_INTEGER = "integer"
+        private const val TYPE_BOOLEAN = "boolean"
+        private const val TYPE_DOUBLE = "double"
+        private const val TYPE_LONG = "long"
+        private const val TYPE_OBJECT = "object"
+        private const val TYPE_JSON = "json"
+        private const val TYPE_XML = "xml"
+        private const val TYPE_NULL = "null"
     }
 
     override fun getId() = ID
+
+    override fun mutate(record: LocalRecordAtts): String {
+        val mutateData = record.toMutateData()
+
+        log.debug {
+            "Mutate bpmn variable: \n${Json.mapper.toPrettyString(mutateData)}"
+        }
+
+        camundaRuntimeService.setVariableLocal(mutateData.executionId, mutateData.name, mutateData.value)
+
+        return record.id.ifBlank {
+            val variableInstance = camundaRuntimeService.createVariableInstanceQuery()
+                .executionIdIn(mutateData.executionId)
+                .variableName(mutateData.name)
+                .singleResult() ?: error("Variable with name '${mutateData.name}' not found")
+
+            variableInstance.id
+        }
+    }
+
+    private fun LocalRecordAtts.toMutateData(): MutateValueData {
+        val id = this.id
+
+        val (executionId: String, name: String) = if (id.isNotBlank()) {
+            val variable = camundaRuntimeService.createVariableInstanceQuery()
+                .variableId(id)
+                .singleResult() ?: error("Variable with id '$id' not found")
+            variable.executionId to variable.name
+        } else {
+            this.getAtt(ATT_EXECUTION_ID).asText() to this.getAtt(ATT_NAME).asText()
+        }
+
+        if (this.id.isBlank() && executionId.isBlank()) {
+            error("You must specify either 'id' or 'executionId' attribute: $this")
+        }
+
+        require(name.isNotBlank()) { "Variable name can't be blank: $this" }
+        require(executionId.isNotBlank()) { "Execution id can't be blank: $this" }
+
+        val objectTypeName = this.getAtt(ATT_OBJECT_TYPE_NAME).asText()
+
+        val type: String? = when (this.getAtt(ATT_TYPE).asText().lowercase()) {
+            TYPE_STRING -> String::class.java.typeName
+            TYPE_DATE -> Date::class.java.typeName
+            TYPE_INTEGER -> Int::class.java.typeName
+            TYPE_BOOLEAN -> Boolean::class.java.typeName
+            TYPE_DOUBLE -> Double::class.java.typeName
+            TYPE_LONG -> Long::class.java.typeName
+            TYPE_OBJECT -> {
+                if (objectTypeName.isBlank()) {
+                    error("Object type name can't be blank for type 'Object': $this")
+                }
+                objectTypeName
+            }
+
+            TYPE_JSON -> error("Json type is not supported for mutation")
+            TYPE_NULL -> null
+            else -> error("Unknown variable type: $this")
+        }
+
+        val valueToMutate: Any? = if (type != null) {
+            val explicitType: JavaType = Json.mapper.getTypeFactory().constructFromCanonical(type)
+                ?: error("Type not constructed from canonical: $this")
+            val rawValue = this.getAtt(ATT_VALUE).asJavaObj() ?: error("Value is null: $this")
+
+            val explicitValue: Any = Json.mapper.convert(rawValue, explicitType)
+                ?: error("Failed to convert value: $rawValue to type: $explicitType")
+
+            explicitValue
+        } else {
+            null
+        }
+
+        return MutateValueData(id, executionId, name, valueToMutate)
+    }
+
+    override fun delete(recordId: String): DelStatus {
+        val variableInstance = camundaRuntimeService.createVariableInstanceQuery()
+            .variableId(recordId)
+            .singleResult() ?: error("Variable with id '$recordId' not found")
+
+        camundaRuntimeService.removeVariable(variableInstance.executionId, variableInstance.name)
+
+        return DelStatus.OK
+    }
 
     override fun queryRecords(recsQuery: RecordsQuery): RecsQueryRes<EntityRef> {
         val predicate = recsQuery.getQuery(Predicate::class.java)
@@ -61,14 +167,6 @@ class BpmnVariableInstanceRecords(
         return result
     }
 
-    override fun getRecordAtts(recordId: String): Any? {
-        return camundaRuntimeService.createVariableInstanceQuery()
-            .variableId(recordId)
-            .singleResult()?.let {
-                BpmnVariableInstanceRecord(it as VariableInstanceEntity)
-            }
-    }
-
     private fun VariableInstanceQuery.applyPredicate(pred: Predicate): VariableInstanceQuery {
         val query = PredicateUtils.convertToDto(pred, BpmnVariableInstanceQuery::class.java)
 
@@ -81,8 +179,9 @@ class BpmnVariableInstanceRecords(
                 variableNameLike("%${query.name}%")
             }
 
-            if (query.activityInstanceId.isNotBlank()) {
-                activityInstanceIdIn(query.activityInstanceId)
+            val scopeId = query.scope.getLocalId()
+            if (scopeId.isNotBlank()) {
+                activityInstanceIdIn(query.scope.getLocalId())
             }
         }
     }
@@ -109,6 +208,14 @@ class BpmnVariableInstanceRecords(
                 desc()
             }
         }
+    }
+
+    override fun getRecordAtts(recordId: String): Any? {
+        return camundaRuntimeService.createVariableInstanceQuery()
+            .variableId(recordId)
+            .singleResult()?.let {
+                BpmnVariableInstanceRecord(it as VariableInstanceEntity)
+            }
     }
 
     private inner class BpmnVariableInstanceRecord(
@@ -146,24 +253,42 @@ class BpmnVariableInstanceRecords(
             return variableInstance.typeName
         }
 
-        @AttName("activityInstanceId")
-        fun getActivityInstanceId(): String {
-            return variableInstance.activityInstanceId
+        @AttName("scope")
+        fun getScope(): EntityRef {
+            val activityInstanceId: String? = variableInstance.activityInstanceId
+
+            if (activityInstanceId.isNullOrBlank()) {
+                return EntityRef.EMPTY
+            }
+
+            return if (activityInstanceId.contains(":")) {
+                EntityRef.create(
+                    AppName.EPROC,
+                    BpmnHistoricActivityInstanceRecords.ID,
+                    activityInstanceId
+                )
+            } else {
+                EntityRef.create(
+                    AppName.EPROC,
+                    BpmnProcessRecords.ID,
+                    activityInstanceId
+                )
+            }
         }
 
         @AttName("value")
         fun getValue(): Any {
             return when (variableInstance.typeName) {
-                "json" -> variableInstance.value?.toString() ?: ""
-                "xml" -> "Xml object"
-                "object" -> variableInstance.textValue2 ?: ""
+                TYPE_JSON -> variableInstance.value?.toString() ?: ""
+                TYPE_XML -> "Xml object"
+                TYPE_OBJECT -> variableInstance.textValue2 ?: ""
                 else -> variableInstance.value ?: ""
             }
         }
 
         @AttName("serializableValue")
         fun getSerializableValue(): SerializableValueData? {
-            if (variableInstance.typeName == "xml") {
+            if (variableInstance.typeName == TYPE_XML) {
                 val xmlValue = variableInstance.byteArrayValue.toString(StandardCharsets.UTF_8)
                 return SerializableValueData(
                     serialized = xmlValue
@@ -183,10 +308,16 @@ class BpmnVariableInstanceRecords(
                 serialized = typedValue.valueSerialized,
                 deserialized = typedValue.value?.toString() ?: ""
             )
-
         }
 
     }
+
+    private data class MutateValueData(
+        val variableId: String,
+        var executionId: String,
+        val name: String?,
+        val value: Any?
+    )
 
     private data class TypedValueInfo(
         var objectTypeName: String = "",
@@ -202,6 +333,6 @@ class BpmnVariableInstanceRecords(
     data class BpmnVariableInstanceQuery(
         var processInstance: EntityRef = EntityRef.EMPTY,
         var name: String = "",
-        var activityInstanceId: String = ""
+        var scope: EntityRef = EntityRef.EMPTY
     )
 }
