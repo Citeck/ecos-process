ae36935cbe3f237711097a83eefbb08f5d3dfc13
Roman Makarskiy <romanchabest55@gmail.com>
2024-01-31 20:08:10 +0300

    ECOSENT-2804 - bpmn kpi
    - exclude bpmn elements from bpmn event subscriptions

--- src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/impl/events/bpmnevents/subscribe/BpmnEventSubscriptionService.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/impl/events/bpmnevents/subscribe/BpmnEventSubscriptionService.kt
@@ -8,11 +8,13 @@ import ru.citeck.ecos.commons.json.Json
 import ru.citeck.ecos.events2.EventsService
 import ru.citeck.ecos.events2.listener.ListenerHandle
 import ru.citeck.ecos.events2.type.RecordChangedEvent
+import ru.citeck.ecos.process.domain.bpmn.elements.config.BPMN_PROCESS_ELEMENT_TYPE
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.impl.events.bpmnevents.*
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.impl.events.bpmnevents.conditional.BpmnConditionalEventsProcessor
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.impl.events.bpmnevents.conditional.RecordUpdatedEvent
 import ru.citeck.ecos.process.domain.bpmn.engine.camunda.impl.events.bpmnevents.publish.CamundaEventProcessor
 import ru.citeck.ecos.records2.predicate.model.Predicates
+import ru.citeck.ecos.webapp.api.constants.AppName
 import ru.citeck.ecos.webapp.api.entity.EntityRef
 import java.time.Instant
 import java.util.*
@@ -64,7 +66,8 @@ class BpmnEventSubscriptionService(
     }
 
     fun addSubscriptionsForDefRev(procDefRevId: UUID) {
-        val deployedSubscriptionsData = camundaEventSubscriptionFinder.getDeployedSubscriptionsDataByProcDefRevId(procDefRevId)
+        val deployedSubscriptionsData =
+            camundaEventSubscriptionFinder.getDeployedSubscriptionsDataByProcDefRevId(procDefRevId)
         val combinedSubscriptions = deployedSubscriptionsData.subscriptions.combine()
 
         log.debug { "Add subscriptions for procDefRevId: $procDefRevId, subscriptions: \n$combinedSubscriptions" }
@@ -124,6 +127,12 @@ class BpmnEventSubscriptionService(
                 withDataClass(ObjectData::class.java)
                 withEventType(listenerEventName)
                 withAttributes(attributes.associateBy { it })
+                // optimization - exclude BPMN_PROCESS_ELEMENT_TYPE from subscription
+                withFilter(
+                    Predicates.notEq(
+                        "record._type?id", "${AppName.EMODEL}/type@$BPMN_PROCESS_ELEMENT_TYPE"
+                    )
+                )
                 withTransactional(true)
                 withAction { event ->
                     log.debug { "Receive subscription event: \n${Json.mapper.toPrettyString(event)}" }
