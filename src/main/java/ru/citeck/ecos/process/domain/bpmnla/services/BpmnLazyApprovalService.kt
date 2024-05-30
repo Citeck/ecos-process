b50728696f1aa10fd60691e17530c424244abfaf
Roman Viryasov <roman.viryasov@citeck.ru>
2024-05-30 15:43:57 +0400

    + ECOSENT-2934 - Failed to execute task through LA

--- src/main/java/ru/citeck/ecos/process/domain/bpmnla/services/BpmnLazyApprovalService.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmnla/services/BpmnLazyApprovalService.kt
@@ -182,12 +182,6 @@ class BpmnLazyApprovalService(
             return fillLazyApprovalReport(MailProcessingCode.TOKEN_NOT_FOUND, task)
         }
 
-        val completeTaskOutcome = task.definitionKey?.let { Outcome(it, outcome.id, outcome.name) }
-        if (completeTaskOutcome == null) {
-            log.warn { "Task with id = $taskId has no outcome = $outcome!" }
-            return fillLazyApprovalReport(MailProcessingCode.OUTCOME_NOT_FOUND, task)
-        }
-
         val outcomeForTask = Outcome.OUTCOME_PREFIX + taskOutcome
         val recordAtt = RecordAtts(EntityRef.create(AppName.EPROC, ProcTaskRecords.ID, taskId))
         recordAtt.setAtt(outcomeForTask, true)
