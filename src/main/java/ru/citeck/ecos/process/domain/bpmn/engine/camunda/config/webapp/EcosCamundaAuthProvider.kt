b5ed2ad4b88ea4a45b520a985f3ce2e23eeb98b7
Roman Makarskiy <romanchabest55@gmail.com>
2024-11-26 12:24:55 +0300

    ECOSENT-3163 - Camunda admin page broken after Springboot 3.0 migration

--- src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/config/webapp/EcosCamundaAuthProvider.kt
+++ src/main/java/ru/citeck/ecos/process/domain/bpmn/engine/camunda/config/webapp/EcosCamundaAuthProvider.kt
@@ -5,22 +5,17 @@ import org.camunda.bpm.engine.ProcessEngine
 import org.camunda.bpm.engine.rest.security.auth.AuthenticationResult
 import org.camunda.bpm.engine.rest.security.auth.impl.ContainerBasedAuthenticationProvider
 import ru.citeck.ecos.context.lib.auth.AuthContext
-import ru.citeck.ecos.context.lib.auth.AuthUser
 
 class EcosCamundaAuthProvider : ContainerBasedAuthenticationProvider() {
 
-    private val deniedUsers = listOf(AuthUser.ANONYMOUS, AuthUser.GUEST, "anonymousUser")
-
     override fun extractAuthenticatedUser(request: HttpServletRequest, engine: ProcessEngine): AuthenticationResult {
-
-        val currentFullAuth = AuthContext.getCurrentFullAuth()
-        val user = currentFullAuth.getUser()
-        if (currentFullAuth.isEmpty() || deniedUsers.contains(user)) {
-            return AuthenticationResult.unsuccessful()
+        if (AuthContext.isRunAsAdmin()) {
+            return AuthenticationResult("admin", true).apply {
+                val currentFullAuth = AuthContext.getCurrentFullAuth()
+                groups = currentFullAuth.getAuthorities()
+            }
         }
 
-        return AuthenticationResult(user, true).apply {
-            groups = currentFullAuth.getAuthorities()
-        }
+        return AuthenticationResult.unsuccessful()
     }
 }
